4d235b32638b795b036ac486356972a7d6482fe5
cofl <cllacour@mtu.edu>
2020-05-09 11:55:47 -0400

    Reverse processing of JS and JSON config files, so JS can register providers for JSON.

--- lib/index.ts
+++ lib/index.ts
@@ -8,18 +8,18 @@ const args = arg({ /* TODO: options */ });
 const cwd = path.resolve(args._[0] ?? '.');
 (async function(){
     const config = new Config({ rootDirectory: cwd });
-    const jsonConfigPath = config.resolveRealFile(`.${config.configTokenFragment}.json`);
-    if(jsonConfigPath){
-        const jsonContent = fs.readFileSync(jsonConfigPath, { encoding: config.defaultEncoding });
-        const jsonConfig = JSON.parse(jsonContent);
-        config.importOptions(jsonConfig);
-    }
     const jsConfigPath = config.resolveRealFile(`.${config.configTokenFragment}.js`);
     if(jsConfigPath)
     {
         const jsConfigurer = require(jsConfigPath);
         const jsConfig = ((typeof jsConfigurer === 'function') ? jsConfigurer(config) : jsConfigurer) || {};
         config.importOptions(jsConfig);
+    }
+    const jsonConfigPath = config.resolveRealFile(`.${config.configTokenFragment}.json`);
+    if(jsonConfigPath){
+        const jsonContent = fs.readFileSync(jsonConfigPath, { encoding: config.defaultEncoding });
+        const jsonConfig = JSON.parse(jsonContent);
+        config.importOptions(jsonConfig);
     }
     const ssgc = new SSGC(config);
     await ssgc.build();
